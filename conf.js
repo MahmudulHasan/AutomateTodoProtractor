cb71863fd427c7791afa7059b69a08d569d48c48
MahmudulHasan <autul.aust@gmail.com>
2016-06-06 07:31:41 +0600

    Added some new config in config.js file

--- conf.js
+++ conf.js
@@ -1,4 +1,12 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./src/basicTest_spec.js']
+  specs: ['./src/basicTest_spec.js'],
+  capabilities: {
+        browserName: 'chrome'
+  },
+  jasmineNodeOpts: {
+        // If true, display spec names.
+        isVerbose: true,
+        showColors: true
+  }
 };
