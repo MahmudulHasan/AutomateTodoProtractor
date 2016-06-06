9fc4642b5934a0b7d0187409caae71b81106bcc6
Mahmudul Hasan Autul <mahmudul.hasan@widespace.com>
2016-06-06 12:00:03 +0600

    Added a new test case.

--- src/basicTest_spec.js
+++ src/basicTest_spec.js
@@ -7,4 +7,24 @@ describe('Basic protractor test', function() {
 		expect(element(by.id('todotask')).isPresent()).toBe(true);
 		expect(element(by.id('addtodotask')).isPresent()).toBe(true);
 	});
+
+	it('Should add a todo task in the todo list',function() {
+		var todoTextBox = element(by.id('todotask'));
+		var addNewButton = element(by.id('addtodotask'));
+
+		// All the list item 
+		var todoList = element.all(by.repeater('x in todoList'));
+		var numOfListItem = null;
+
+		// Count list item then add another todo task list item no will increase by 1
+		todoList.count().then(function(result) {
+			console.log(result); 
+			numOfListItem = result;
+
+			// Add another todo task
+			todoTextBox.sendKeys("Second todo task");
+			addNewButton.click();
+			expect(todoList.count()).toEqual(numOfListItem+1);
+		});
+	});
 });
